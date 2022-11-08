6effc61da4eaed035a64e6fb61906c718ac17813
GiuseppeRepici <giuseppe.repici95@gmail.com>
2022-11-08 13:58:16 +0100

    esercizio 1 ok

---
+++ js/pali.js
@@ -0,0 +1,31 @@
+const parolaUtente = prompt("inserisci una parola!");
+
+
+const arrayParolaOk = parolaUtente.split("");
+const arrayParola = parolaUtente.split("");
+const arrayInvertito = arrayParola.reverse();
+let controllopali = false;
+
+console.log(arrayParolaOk);
+console.log(arrayInvertito);
+
+function controllopalindroma() {
+    for (let i=0 ; i < arrayParolaOk.length; i++){
+
+        if (arrayParolaOk[i] === arrayInvertito[i]){
+            controllopali = true;
+        } else 
+            controllopali = false;
+            i = 100;
+        
+    }
+
+}
+
+controllopalindroma();
+
+if (controllopali === true){
+    let result = alert("è poli");
+} else {
+    let result = alert("non è poli");
+}
